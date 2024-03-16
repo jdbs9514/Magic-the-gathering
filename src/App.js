1388d6562207233e7a0f8cdfb30c4c8a90f7dd3a
Josue Benavides <jdbs9514@gmail.com>
2024-03-15 20:20:03 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home  from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import mtg from "../assets/images/magic-logo.webp";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between bg-zinc-900 p-1">
+      <div className="ml-6">
+        <Link to="/">
+          <img src={mtg} alt="logo" className="w-[50%]" />
+        </Link>
+      </div>
+      <div className="mr-4 mt-3">
+        <button type="button" className="text-white">
+          <i className="fa-solid fa-bars"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
