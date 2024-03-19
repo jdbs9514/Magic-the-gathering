9a1cfe97611097967de4e5397628457a74d0a503
Josue Benavides <jdbs9514@gmail.com>
2024-03-19 01:27:33 -0500

    fix the img errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,28 @@
 import { useEffect } from "react";
 import { fetchApi } from "../store/slices/cards/cardslice";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-
-  const cards = useSelector(state => state.cards.list);
+  const cards = useSelector((state) => state.cards.list);
   const dispatch = useDispatch();
-
 
   useEffect(() => {
     dispatch(fetchApi());
   }, [dispatch]);
 
-
   return (
     <div>
       <div className="grid grid-cols-2">
-        {
-          cards.map(card => (
-            <div key={card.id} className="w-[50%]">
-              <img src={card.imageUrl} alt="card" className="w-[50%]" />
-            </div>
-          ))
-        };
+        {cards.map(
+          (card) =>
+          // con esta condicion solo se mostrar las imagenes que si lleguen a renderizar
+            card.imageUrl && (
+              <div key={card.id} className="w-[50%]">
+                <img src={card.imageUrl} alt="card" className="w-[50%]" />
+              </div>
+            )
+        )}
+        ;
       </div>
     </div>
   );
