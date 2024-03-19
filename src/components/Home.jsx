2c837d1de3a704239c295eaf63ac9b65d877475f
Josue Benavides <jdbs9514@gmail.com>
2024-03-19 18:07:20 -0500

    added bg gradient

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,14 +11,14 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <div className="grid grid-cols-2">
+    <div className="bg-gradient-to-r from-zinc-900">
+      <div className="grid grid-cols-4">
         {cards.map(
           (card) =>
           // con esta condicion solo se mostrar las imagenes que si lleguen a renderizar
             card.imageUrl && (
-              <div key={card.id} className="w-[50%]">
-                <img src={card.imageUrl} alt="card" className="w-[50%]" />
+              <div key={card.id} className="w-[90%] flex justify-center m-1 mt-4">
+                <img src={card.imageUrl} alt="card" className="w-[80%]" />
               </div>
             )
         )}
